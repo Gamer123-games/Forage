767cd68fe7b3241a5cef180035e598f57aa91071
Gamer123-games <Austin2daB@hotmail.com>
2021-03-24 12:23:26 -0400

    CODE

---
+++ scripts/Foorage.js
@@ -0,0 +1,16 @@
+var Inventory;
+var Edible;
+var Item;
+var Inventoryspace;
+Inventoryspace = 10;
+function Grab(Inventoryspace = 10){
+    Item.Inventory + 1;
+    Item.Inventoryspace - 1;
+    
+}
+function Forage(){
+    Item.GrenBerry.Edible = true;
+    Item.GrenBerry.Inventory + 1;
+
+}
+function componet()
